f7cbbc960ac0898d95bd2ed9e8100cd74860f34d
w7vivek <wvivek.181@gmail.com>
2025-09-18 09:33:57 +0530

    The Product Hub update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Login from "./Components/Login";
+// import Home from "./Components/Home";
+// import Vegetables from "./Components/Vegetables";
+// import Fruits from "./Components/Fruits";
+// import Layout from "./Components/Layout";
+// import Cart from "./Components/Cart";
+// import Aboutus from "./Components/Aboutus";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route element={<Layout />}>
+//           <Route path="/main_page" element={<Home />} />
+//           <Route path="/vegetables" element={<Vegetables />} />
+//           <Route path="/fruits" element={<Fruits />} />
+//           <Route path="/cart_buy" element={<Cart />} />
+//           <Route path="/about" element={<Aboutus />} />  {/* lowercase and consistent */}
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
@@ -10,15 +41,15 @@ import Aboutus from "./Components/Aboutus";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ProductHub">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route element={<Layout />}>
-          <Route path="/main_page" element={<Home />} />
-          <Route path="/vegetables" element={<Vegetables />} />
-          <Route path="/fruits" element={<Fruits />} />
-          <Route path="/cart_buy" element={<Cart />} />
-          <Route path="/about" element={<Aboutus />} />  {/* lowercase and consistent */}
+          <Route path="main_page" element={<Home />} />
+          <Route path="vegetables" element={<Vegetables />} />
+          <Route path="fruits" element={<Fruits />} />
+          <Route path="cart_buy" element={<Cart />} />
+          <Route path="about" element={<Aboutus />} />
         </Route>
       </Routes>
     </Router>
