c6f64a5e6ab7a5918678cf80e740145fcfea6590
w7vivek <wvivek.181@gmail.com>
2025-09-18 10:43:37 +0530

    The Product Hub update1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,3 @@
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./Components/Login";
-// import Home from "./Components/Home";
-// import Vegetables from "./Components/Vegetables";
-// import Fruits from "./Components/Fruits";
-// import Layout from "./Components/Layout";
-// import Cart from "./Components/Cart";
-// import Aboutus from "./Components/Aboutus";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route element={<Layout />}>
-//           <Route path="/main_page" element={<Home />} />
-//           <Route path="/vegetables" element={<Vegetables />} />
-//           <Route path="/fruits" element={<Fruits />} />
-//           <Route path="/cart_buy" element={<Cart />} />
-//           <Route path="/about" element={<Aboutus />} />  {/* lowercase and consistent */}
-//         </Route>
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
